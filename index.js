45489d7ecd13fa4fc7e612aad765f5b7ba7d9da9
Pancham Mehta <pancham1305@gmail.com>
2023-05-11 14:36:45 +0530

    UPdates

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/", (req, res) => {
         }
         data.push(d.videos[i]);
       }
-      
+
       res.render("search", { data });
     })
     .catch((e) => {
@@ -42,26 +42,28 @@ app.post("/", (req, res) => {
       res.render("search", { data });
     });
 });
-  app.get("/play/:id", (req, res) => {
-    const { id } = req.params;
+app.get("/play/:id", (req, res) => {
+  const { id } = req.params;
 
-    const info = youtube
-      .getBasicInfo(id)
-      .then((e) => {
-        // console.log(e);
-        const format = e.chooseFormat({ type: "video+audio", quality: "best" });
-        const url = format?.decipher(youtube.session.player);
-        const base = e.basic_info;
+  const info = youtube
+    .getBasicInfo(id)
+    .then(async (e) => {
+      // console.log(e);
+      const format = e.chooseFormat({ type: "video+audio", quality: "best" });
+      const url = format?.decipher(youtube.session.player);
+      const base = e.basic_info;
+      const channel_id = base.channel_id;
+      const channelInfo = await youtube.getChannel(channel_id);
+      const videos = await channelInfo.getVideos();
+      const array_of_videos = videos.current_tab.content.contents;
+      res.render("player", { url, base, array_of_videos });
+    })
+    .catch((e) => {
+      // console.log(e);
+      res.send("error");
+    });
+});
 
-        res.render("player", { url, base });
-      })
-      .catch((e) => {
-        // console.log(e);
-        res.send("error");
-      });
-  });
-  
-  app.listen(process.env.PORT || 44444, (e) => {
-    if (e) throw e;
-  });
-
+app.listen(process.env.PORT || 44444, (e) => {
+  if (e) throw e;
+});
--- public/app.js
+++ public/app.js
@@ -11,3 +11,12 @@ const cards = document.querySelectorAll(".hidden");
 cards.forEach((e) => observer.observe(e));
 
 
+const search = document.querySelector('.input');
+search.addEventListener('input', async(e) => {
+  const data = fetch("http://localhost:44444/auto",
+    {
+      method: "POST", 
+      
+    }
+  )
+})
